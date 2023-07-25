f5f664ec7746983dd3b89e147886d2f622dd0c3b
Vadim CRM <128800269+mr-dorian-black@users.noreply.github.com>
2023-07-25 14:04:27 +0300

    v1.3

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,67 @@
+import Notiflix from 'notiflix';
+import "notiflix/dist/notiflix-3.2.6.min.css";
+
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
   if (shouldResolve) {
-    // Fulfill
+    return Promise.resolve({ position, delay });
   } else {
-    // Reject
+    return Promise.reject({ position, delay });
   }
 }
+
+const form = document.querySelector('.form');
+
+let {
+  elements: {
+    amount,
+    delay,
+    step
+  }
+} = form;
+
+
+form.addEventListener('submit', (event) => {
+  event.preventDefault();
+  let delayInt = Number(delay.value);
+  let stepInt = Number(step.value);
+  let amountInt = Number(amount.value);
+  let position = 1;
+
+  setTimeout(function run() {
+
+    createPromise(position, delayInt).then(({ position, delay }) => {
+      Notiflix.Notify.success(
+        `✅ Fulfilled promise ${position} in ${delay}ms`,
+        {
+          timeout: 3000,
+          useIcon: false
+        },
+      );
+    })
+      .catch(({ position, delay }) => {
+        Notiflix.Notify.failure(
+          `❌ Rejected promise ${position} in ${delay}ms`,
+          {
+            timeout: 3000,
+            useIcon: false
+          },
+        );
+      });
+
+    position++;
+    delayInt += stepInt;
+
+    if (position <= amountInt) {
+      setTimeout(run, stepInt);
+    }
+    else {
+      delayInt = Number(delay.value);
+      stepInt = Number(step.value);
+      amountInt = Number(amount.value);
+      position = 1;
+    }
+
+  }, delayInt);
+
+})
